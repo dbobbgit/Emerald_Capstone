5c2c642a366b05447c03b604f5f388c8ccb6f87a
Caitlin Golden <caityladygolden@gmail.com>
2021-07-07 16:52:32 -0500

    will have model built, view and template constructed with basic user info and no formatting. Model is registered in admin.

--- user/views.py
+++ user/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from user.models import CustomUser
 # Create your views here.
+
+
+def profile_view(request, user_id:int):
+    current_user = CustomUser.objects.get(id=user_id)
+    return render(request, 'profile.html', {'current_user':current_user })
