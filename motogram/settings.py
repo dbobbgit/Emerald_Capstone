bbe62e19fe64d52dc84295deddc7b27d27f5a699
dbobbgit <63315747+dbobbgit@users.noreply.github.com>
2021-07-08 10:59:32 -0700

    resolves merge conflicts in motogram settings.py and urls.py

--- motogram/settings.py
+++ motogram/settings.py
@@ -38,11 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
-    'user',
-=======
     'moto_user',
->>>>>>> user
     'authentication',
     'post',
     'notifications',
