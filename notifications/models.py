8c0b74ceaedfee21c18b1433ab8281649c7e3b30
dbobbgit <63315747+dbobbgit@users.noreply.github.com>
2021-07-14 12:34:24 -0700

    restores .gitignore to main

---
+++ notifications/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from moto_user.models import MotoUser
+# from post.models import Post
+
+# Create your models here.
+class Notifications(models.Model):
+    author = models.ForeignKey(
+        MotoUser, 
+        on_delete=models.CASCADE)
+    text = models.ForeignKey(
+        'post.Post', 
+        on_delete=models.CASCADE)
+    read = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True, null = True)
+    
