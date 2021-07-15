cdbe69b721c57e66961a7357a458c42234161b36
Danielle Bobb <danielle@oakandivymedia.com>
2021-07-15 11:07:14 -0700

    updates notification model

--- notification/models.py
+++ notification/models.py
@@ -2,18 +2,15 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from moto_user.models import MotoUser
-# from post.models import Post
 
 
 # Create your models here.
 class Notification(models.Model):
-    title = models.CharField(max_length=100, blank=True)
-    message = models.TextField(default=False)
-    author = models.ForeignKey(
+    receiver = models.ForeignKey(
         MotoUser,
         on_delete=models.CASCADE)
-    text = models.ForeignKey(
+    post_to_notify = models.ForeignKey(
         'post.Post',
         on_delete=models.CASCADE)
     read = models.BooleanField(default=False)
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
